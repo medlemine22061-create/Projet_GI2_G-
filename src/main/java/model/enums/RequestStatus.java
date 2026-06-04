f58bf1a31e377d3d3f00fe0f290702e7e0b61007
Yousef <2copycat4@gmail.com>
2026-06-04 19:47:07 +0200

    Adding new package

---
+++ src/main/java/model/enums/RequestStatus.java
@@ -0,0 +1,12 @@
+package model.enums;
+
+/**
+ * Represents the status of a delivery request.
+ */
+public enum RequestStatus {
+    PENDING,
+    VALIDATED,
+    ASSIGNED,
+    COMPLETED,
+    CANCELLED
+}
